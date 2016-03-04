c5dd38cb07edba87b62f9372f4f535e19c8ca8a0
Rene Enriquez <renriquez@ioet.com>
2016-03-04 09:47:16 -0600

    Login quemado y refactorizacion hacia servicios angular JS

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,19 @@
 angular.module('starter.controllers', [])
 
-  .controller('LoginController', function ($scope, $state) {
+  .controller('LoginController', function ($scope, $state, LoginService) {
 
-    $scope.signUp = function(){
+    $scope.signUp = function () {
       $state.go('sign-up');
     }
 
     $scope.login = function (user) {
-      console.log('Login', user)
-      $state.go('tab.home');
+      if (LoginService.login(user)) {
+        $scope.login.error = false;
+        $scope.user = null;
+        $state.go('tab.home');
+      } else {
+        $scope.login.error = true;
+      }
     };
 
   })
@@ -16,78 +21,54 @@ angular.module('starter.controllers', [])
   .controller('DashCtrl', function ($scope) {
   })
 
-  .controller('ChatsCtrl', function ($scope, $cordovaCamera, $cordovaFileTransfer, $ionicLoading, $ionicPopup, $state, $cordovaGeolocation) {
+  .controller('ChatsCtrl', function (PictureService, GeoLocationService, $ionicPlatform, $scope, $cordovaCamera, $cordovaFileTransfer, $ionicLoading, $ionicPopup, $state, $cordovaGeolocation) {
+
+    $scope.findGeolocation = function () {
+      GeoLocationService.getCurrentLocation().then(function (currentLocation) {
+        $scope.currentPosition = currentLocation;
+      });
+    }
+
+    $ionicPlatform.ready(function () {
+      $scope.findGeolocation();
+    })
 
 
-    var posOptions = {
-      enableHighAccuracy: true,
-      timeout: 20000,
-      maximumAge: 0
-    };
+
 
     $scope.takePicture = function () {
-      var options = {
-        quality: 80,
-        destinationType: Camera.DestinationType.DATA_URL,
-        sourceType: Camera.PictureSourceType.CAMERA,
-        allowEdit: true,
-        encodingType: Camera.EncodingType.JPEG,
-        targetWidth: 300,
-        targetHeight: 300,
-        popoverOptions: CameraPopoverOptions,
-        saveToPhotoAlbum: false,
-        correctOrientation: true
-      };
+      //alert('taking picture');
+      var options = PictureService.getCameraOptions();
 
-      var optionsUp = {
-        fileKey: "key",
-        fileName: "image.jpeg",
-        chunkedMode: "false",
-        mimeType: "image/jpeg"
-      };
 
       $scope.sendPhoto = function () {
-
-        var lat = -1;
-        var long = -1;
-
-        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
-          lat = position.coords.latitude;
-          long = position.coords.longitude;
-
-
-          $cordovaFileTransfer.upload("https://demoodra.herokuapp.com/uploadImage?lat="+lat+"&long="+long, $scope.imgUri, optionsUp)
-            .then(function (result) {
-              $ionicLoading.hide();
-              var alertPopup = $ionicPopup.alert({
-                title: 'Aviso',
-                template: 'Los datos han sido enviados exitosamente',
-                okType: 'button-dark'
-              });
-
-              alertPopup.then(function (res) {
-                $state.go('tab.home');
-              });
-
-            }, function (err) {
-              alert('Error: ' + JSON.stringify(err))
-            }, function (progress) {
-              //$ionicLoading.show();
+        var optionsUp = PictureService.getUploadOptions();
+        //alert('sending picture');
+        $cordovaFileTransfer.upload("https://demoodra.herokuapp.com/uploadImage?lat=" + $scope.currentPosition.lat + "&long=" + $scope.currentPosition.long, $scope.imgUri, optionsUp)
+          .then(function (result) {
+            $ionicLoading.hide();
+            var alertPopup = $ionicPopup.alert({
+              title: 'Aviso',
+              template: 'Los datos han sido enviados exitosamente',
+              okType: 'button-dark'
             });
 
-        }, function (err) {
-          console.log(err);
-        }), function (progress) {
-          $ionicLoading.show();
-        };
+            alertPopup.then(function (res) {
+              $state.go('tab.home');
+            });
 
+          }, function (err) {
+            alert('Error: ' + JSON.stringify(err))
+          }, function (progress) {
+            //$ionicLoading.show();
+          });
       }
 
 
       $cordovaCamera.getPicture(options).then(function (imageData) {
         $scope.imgUri = "data:image/jpeg;base64," + imageData;
       }, function (err) {
-        // error
+        console.log(err);
       });
 
     }
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,113 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
+  .service('LoginService', function () {
+    this.login = function (user) {
+      return user != null && user.email == user.password;
     }
-  };
-});
+  })
+
+  .service('GeoLocationService', function ($cordovaGeolocation) {
+
+    var currentLocation = [];
+    var gpsOptions = {
+      enableHighAccuracy: true,
+      timeout: 20000,
+      maximumAge: 0
+    };
+
+    return {
+      getCurrentLocation: function () {
+        return $cordovaGeolocation.getCurrentPosition(gpsOptions).then(function (position) {
+          currentLocation.lat = position.coords.latitude;
+          currentLocation.long = position.coords.longitude;
+          return currentLocation;
+        });
+      }
+
+    }
+
+  })
+
+  .service('PictureService', function ($cordovaGeolocation) {
+
+    var optionsFileUpload = {
+      fileKey: "key",
+      fileName: "image.jpeg",
+      chunkedMode: "false",
+      mimeType: "image/jpeg"
+    };
+
+    var options = {
+      quality: 80,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+
+    return {
+      getCameraOptions: function () {
+        return options;
+      },
+
+      getUploadOptions: function () {
+        return optionsFileUpload;
+      }
+
+    }
+
+  })
+
+  .factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var chats = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'img/ben.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      face: 'img/perry.png'
+    }, {
+      id: 4,
+      name: 'Mike Harrington',
+      lastText: 'This is wicked good ice cream.',
+      face: 'img/mike.png'
+    }];
+
+    return {
+      all: function () {
+        return chats;
+      },
+      remove: function (chat) {
+        chats.splice(chats.indexOf(chat), 1);
+      },
+      get: function (chatId) {
+        for (var i = 0; i < chats.length; i++) {
+          if (chats[i].id === parseInt(chatId)) {
+            return chats[i];
+          }
+        }
+        return null;
+      }
+    };
+  });
