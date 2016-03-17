c315b42bd6a0cb9c2791b13cc40b1f964c088fcf
Rene Enriquez <enriquezrene.ap@gmail.com>
2016-03-16 22:29:08 -0500

    Camera working on ios and android devices

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,160 +1,150 @@
 angular.module('starter.controllers', [])
 
-  .controller('LoginController', function ($scope, $http, $state, $ionicModal, LoginService) {
+.controller('LoginController', function ($scope, $http, $state, $ionicModal, LoginService) {
 
     clearForm();
 
     $scope.showSignUpForm = function () {
-      clearForm();
-      $state.go('sign-up');
+        clearForm();
+        $state.go('sign-up');
     };
 
     $scope.login = function () {
-      LoginService.login($scope.user).then(function (data) {
-        clearForm();
-        $state.go('tab.home');
-      }).catch(function (error) {
-        clearForm();
-        $scope.message = 'Credenciales incorrectas';
-      })
+        LoginService.login($scope.user).then(function (data) {
+            clearForm();
+            $state.go('tab.home');
+        }).catch(function (error) {
+            clearForm();
+            $scope.message = 'Credenciales incorrectas';
+        })
     };
 
     function clearForm() {
-      $scope.message = undefined;
-      $scope.user = {
-        email: null,
-        password: null,
-        name: null
-      };
+        $scope.message = undefined;
+        $scope.user = {
+            email: null,
+            password: null,
+            name: null
+        };
     }
 
-  })
+})
 
-  .controller('SignUpController', function ($scope, $state, LoginService) {
+.controller('SignUpController', function ($scope, $state, LoginService) {
 
     clearForm();
 
     $scope.registerUser = function () {
-      LoginService.createUser($scope.user).then(function (data) {
-        clearForm();
-        $state.go('tab.home');
-      }).catch(function (error) {
-        clearForm();
-        $scope.message = 'Ocurrio un error al crear el usuario: ';
-      })
+        LoginService.createUser($scope.user).then(function (data) {
+            clearForm();
+            $state.go('tab.home');
+        }).catch(function (error) {
+            clearForm();
+            $scope.message = 'Ocurrio un error al crear el usuario: ';
+        })
     };
 
     function clearForm() {
-      $scope.message = undefined;
-      $scope.user = {
-        email: null,
-        password: null,
-        name: null
-      };
+        $scope.message = undefined;
+        $scope.user = {
+            email: null,
+            password: null,
+            name: null
+        };
     }
 
-  })
+})
 
-  .controller('DashCtrl', function ($scope, $http, LoginService) {
+.controller('DashCtrl', function ($scope, $http, LoginService) {
 
-  })
+})
 
-  .controller('ChatsCtrl', function (PictureService, GeoLocationService, $ionicPlatform, $scope, $cordovaCamera, $cordovaFileTransfer, $ionicLoading, $ionicPopup, $state, $cordovaGeolocation) {
+.controller('ChatsCtrl', function (PictureService, GeoLocationService, $ionicPlatform, $scope, $cordovaCamera, $cordovaFileTransfer, $ionicLoading, $ionicPopup, $state, $cordovaGeolocation) {
+
+    clearData();
 
     $scope.findGeolocation = function () {
-      GeoLocationService.getCurrentLocation()
-        .then(function (currentLocation) {
-          $scope.currentPosition = currentLocation;
-        });
+        alert('finding loc');
+        GeoLocationService.getCurrentLocation()
+            .then(function (currentLocation) {
+                $scope.currentPosition = currentLocation;
+            });
     };
 
     $ionicPlatform.ready(function () {
-      $scope.findGeolocation();
+        $scope.findGeolocation();
     });
 
-    $scope.item = {
-      data: "",
-      imagePath: "Photo capture as Base64",
-      destinationFILE_URI: false
-    };
-
     $scope.getPicture = function () {
-      var options = {
-        quality: 50,
-        allowEdit: true,
-        correctOrientation: false,
-        targetWidth: 284,
-        targetHeight: 309,
-        imagePath : "Photo capture as Base64",
-        destinationType: Camera.DestinationType.DATA_URL,
-        encodingType: Camera.EncodingType.JPEG,
-        saveToPhotoAlbum: false
-      };
+        var options = {
+            quality: 50,
+            allowEdit: true,
+            correctOrientation: false,
+            targetWidth: 284,
+            targetHeight: 309,
+            imagePath: "Photo capture as Base64",
+            destinationType: Camera.DestinationType.DATA_URL,
+            encodingType: Camera.EncodingType.JPEG,
+            saveToPhotoAlbum: false
+        };
 
-      $cordovaCamera.getPicture(options).then(function (imageData) {
-        $scope.item.imagePath = "Photo capture as Base64";
-        $scope.item.data = "data:image/jpeg;base64," + imageData;
-        console.log(imageData);
-      }, function (err) {
-        alert('Unable to take picture');
-      });
+        $cordovaCamera.getPicture(options).then(function (imageData) {
+            $scope.item.imagePath = "Photo capture as Base64";
+            $scope.item.data = "data:image/jpeg;base64," + imageData;
+            console.log(imageData);
+        }, function (err) {
+            alert('Unable to take picture');
+        });
     };
 
+    $scope.sendInformation = function () {
+        var optionsUp = PictureService.getUploadOptions();
+        //        params = new Object();
+        //        params.headers = {
+        //            Basic: 'cmVuZTplbnJpcXVleg=='
+        //        };
+        //        optionsUp.params = params;
 
-    //
-    //
-    //$scope.takePicture = function () {
-    //
-    //  var options = PictureService.getCameraOptions();
-    //
-    //  $scope.sendPhoto = function () {
-    //    var optionsUp = PictureService.getUploadOptions();
-    //    alert('sending picture');
-    //    params = new Object();
-    //    params.headers = {Basic: 'cmVuZTplbnJpcXVleg=='};
-    //    optionsUp.params = params;
-    //
-    //    $cordovaFileTransfer.upload("http://diyboot-moe.rhcloud.com/uploadImage?lat=" + $scope.currentPosition.lat + "&long=" + $scope.currentPosition.long, $scope.imgUri, optionsUp)
-    //      .then(function (result) {
-    //        alert('sending picture.');
-    //        $ionicLoading.hide();
-    //        var alertPopup = $ionicPopup.alert({
-    //          title: 'Aviso',
-    //          template: 'Los datos han sido enviados exitosamente',
-    //          okType: 'button-dark'
-    //        });
-    //        alert('sending picture..');
-    //        alertPopup.then(function (res) {
-    //          $state.go('tab.home');
-    //        });
-    //
-    //      }, function (err) {
-    //        alert('Error: ' + JSON.stringify(err))
-    //      }, function (progress) {
-    //        alert('sending picture...');
-    //        $ionicLoading.show();
-    //      });
-    //  }
+        $cordovaFileTransfer.upload("https://demoodra.herokuapp.com/odra/upload?lat=" + $scope.currentPosition.lat + "&long=" + $scope.currentPosition.long, $scope.item.data, optionsUp)
+            .then(function (result) {
+                $ionicLoading.hide();
+                clearData();
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Aviso',
+                    template: 'Los datos han sido enviados exitosamente',
+                    okType: 'button-dark'
+                });
+                alertPopup.then(function (res) {
+                    $state.go('tab.home');
+                });
 
+            }, function (err) {
+                alert('Error: ' + JSON.stringify(err))
+            }, function (progress) {
+                $ionicLoading.show();
+            });
+    };
 
-    //$cordovaCamera.getPicture(options).then(function (imageData) {
-    //  $scope.imgUri = "data:image/jpeg;base64," + imageData;
-    //}, function (err) {
-    //  console.log(err);
-    //});
-
-    //}
-  })
-
-  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
-    $scope.chat = Chats.get($stateParams.chatId);
-  })
-
-  .controller('TabsCtrl', function ($scope, $state, LoginService) {
-    $scope.logout = function () {
-      LoginService.logout();
-      $state.go('login');
+    function clearData() {
+        $scope.item = {
+            data: "",
+            imagePath: "Photo capture as Base64",
+            destinationFILE_URI: false
+        };
     }
 
 
-  });
+})
+
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('TabsCtrl', function ($scope, $state, LoginService) {
+    $scope.logout = function () {
+        LoginService.logout();
+        $state.go('login');
+    }
+
+
+});
--- www/js/services.js
+++ www/js/services.js
@@ -1,140 +1,143 @@
 angular.module('starter.services', ['ngStorage'])
 
 
-  .service('LoginService', function ($http, $localStorage, $q) {
+.service('LoginService', function ($http, $localStorage, $q) {
 
     return {
 
-      login: function (user) {
-        var deferred = $q.defer();
-        var promise = deferred.promise;
-        $http.post('https://demoodra.herokuapp.com/odra/login', user, {
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
-          .success(function (data) {
-            $localStorage['token'] = data;
-            deferred.resolve(data);
-          })
-          .error(function (data, status, headers, config) {
-            deferred.reject(data);
-          });
-        return promise;
-      },
+        login: function (user) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+            $http.post('https://demoodra.herokuapp.com/odra/login', user, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .success(function (data) {
+                    $localStorage['token'] = data;
+                    deferred.resolve(data);
+                })
+                .error(function (data, status, headers, config) {
+                    deferred.reject(data);
+                });
+            return promise;
+        },
 
-      logout: function (user) {
+        logout: function (user) {
 
-      },
+        },
 
-      createUser: function (user) {
-        var deferred = $q.defer();
-        var promise = deferred.promise;
-        $http.post('https://demoodra.herokuapp.com/odra/signup', user, {
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
-          .success(function (data) {
-            $localStorage['token'] = data;
-            deferred.resolve(data);
-          })
-          .error(function (error) {
-            deferred.reject(error);
-          });
-        return promise;
-      }
+        createUser: function (user) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+            $http.post('https://demoodra.herokuapp.com/odra/signup', user, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                .success(function (data) {
+                    $localStorage['token'] = data;
+                    deferred.resolve(data);
+                })
+                .error(function (error) {
+                    deferred.reject(error);
+                });
+            return promise;
+        }
     }
 
-  })
+})
 
-  .service('GeoLocationService', function ($cordovaGeolocation) {
-
+.service('GeoLocationService', function ($cordovaGeolocation) {
+    alert('finding loc in service');
     var currentLocation = [];
     var gpsOptions = {
-      enableHighAccuracy: true,
-      timeout: 20000,
-      maximumAge: 0
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0
     };
 
     return {
-      getCurrentLocation: function () {
-        return $cordovaGeolocation.getCurrentPosition(gpsOptions).then(function (position) {
-          currentLocation.lat = position.coords.latitude;
-          currentLocation.long = position.coords.longitude;
-          return currentLocation;
-        });
-      }
+        getCurrentLocation: function () {
+            alert('finding...');
+            return $cordovaGeolocation.getCurrentPosition(gpsOptions).then(function (position) {
+                alert(position.coords.latitude);
+                currentLocation.lat = position.coords.latitude;
+                alert(position.coords.longitude);
+                currentLocation.long = position.coords.longitude;
+                return currentLocation;
+            });
+        }
 
     }
 
-  })
+})
 
-  .service('PictureService', function ($cordovaGeolocation) {
+.service('PictureService', function ($cordovaGeolocation) {
 
     var optionsFileUpload = {
-      fileKey: "key",
-      fileName: "image.jpeg",
-      chunkedMode: "false",
-      mimeType: "image/jpeg"
+        fileKey: "key",
+        fileName: "image.jpeg",
+        chunkedMode: "false",
+        mimeType: "image/jpeg"
     };
 
     var options = {
-      quality: 80,
-      destinationType: Camera.DestinationType.DATA_URL,
-      sourceType: Camera.PictureSourceType.CAMERA,
-      allowEdit: true,
-      encodingType: Camera.EncodingType.JPEG,
-      targetWidth: 300,
-      targetHeight: 300,
-      popoverOptions: CameraPopoverOptions,
-      saveToPhotoAlbum: false,
-      correctOrientation: true
+        quality: 80,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 300,
+        targetHeight: 300,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false,
+        correctOrientation: true
     };
 
     return {
-      getCameraOptions: function () {
-        return options;
-      },
+        getCameraOptions: function () {
+            return options;
+        },
 
-      getUploadOptions: function () {
-        return optionsFileUpload;
-      }
+        getUploadOptions: function () {
+            return optionsFileUpload;
+        }
 
     }
 
-  })
+})
 
-  .factory('Chats', function () {
+.factory('Chats', function () {
     // Might use a resource here that returns a JSON array
 
     // Some fake testing data
     var chats = [{
-      id: 0,
-      name: 'Ben Sparrow',
-      lastText: 'You on your way?',
-      face: 'img/ben.png'
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'img/ben.png'
     }, {
-      id: 1,
-      name: 'Max Lynx',
-      lastText: 'Hey, it\'s me',
-      face: 'img/max.png'
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'img/max.png'
     }];
 
     return {
-      all: function () {
-        return chats;
-      },
-      remove: function (chat) {
-        chats.splice(chats.indexOf(chat), 1);
-      },
-      get: function (chatId) {
-        for (var i = 0; i < chats.length; i++) {
-          if (chats[i].id === parseInt(chatId)) {
-            return chats[i];
-          }
+        all: function () {
+            return chats;
+        },
+        remove: function (chat) {
+            chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function (chatId) {
+            for (var i = 0; i < chats.length; i++) {
+                if (chats[i].id === parseInt(chatId)) {
+                    return chats[i];
+                }
+            }
+            return null;
         }
-        return null;
-      }
     };
-  });
+});
